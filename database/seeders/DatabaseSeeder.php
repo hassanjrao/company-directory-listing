4c96744f4ef14e53e7d9f68f2b95e41c7f7f4bf9
hassan <hassan@thiqatech.com>
2023-08-28 01:30:04 +0400

    finishinh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            CompanySeeder::class,
-            CompanyRatingSeeder::class,
+            // CompanySeeder::class,
+            // CompanyRatingSeeder::class,
         ]);
     }
 }
